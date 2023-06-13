7f51e544778d54d0fe8a60d627a2d1b75d88e4aa
ubuntu <2546786270@qq.com>
2023-06-13 16:17:37 +0800

    begin config

---
+++ skt/config/config.h
@@ -0,0 +1,14 @@
+#ifndef LEARNWEB_CONFIG_H
+#define LEARNWEB_CONFIG_H
+
+#include "memory"
+#include "sstream"
+#include "boost/lexical_cast.hpp" // 主要用于数值与字符串的相互转换。boost 的 lexical_cast 能把字符串转成各种 c++ 内置类型
+
+namespace skt{
+    
+}
+
+
+
+#endif //LEARNWEB_UTIL_H
